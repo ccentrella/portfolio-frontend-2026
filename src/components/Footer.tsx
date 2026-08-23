f0bf78a2aa6e5ca496c895b2eb3e4f526f77800e
Chris Centrella <usa4cc@gmail.com>
2026-08-23 00:05:13 -0400

    Update Footer.tsx
    Fix broken footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,16 @@
 const Footer = () => (
 	<div className={'snap-start min-h-[100lvh] bg-[#204F5C] text-[#FFFFFF66] p-14 pt-24 pb-10 flex flex-col gap-12 justify-between'}>
 		<div className={"space-y-5 basis-1/5 max-md:basis-full"}>
-			<p className={"text-cyan-500 hover:text-cyan-400 font-medium group text-4xl mb-10 max-sm:mb-8"}><a href={'#'}>Chris <span
+			<p className={"text-cyan-500 hover:text-cyan-400 font-medium group text-4xl mb-10 max-sm:mb-8"}><a href={'/'}>Chris <span
 				className={"ml-2 bg-cyan-200 group-hover:bg-cyan-100 size-2.5 rounded-[50%] inline-block"}></span></a></p>
 			<div className={'flex flex-wrap gap-12 max-sm:gap-10'}>
 				<div
 					className={'font-poppins font-medium text-4xl max-sm:text-3xl text-gray-400 [&_a]:hover:text-gray-300 [&_a]:transition-colors space-y-5 max-sm:space-y-4 basis-2/5 max-md:basis-3/5 max-sm:basis-full'}>
 					<p><a href={'https://blog.chriscentrella.com'} target={'_blank'}>blog</a></p>
-					<p><a href={'#resume'}>resume</a></p>
-					<p><a href={'#chat'}>chat</a></p>
-					<p><a href={'#human-rights'}>human rights</a></p>
-					<p><a href={'#invite-chris'}>invite Chris</a></p>
+					<p><a href={'/#resume'}>resume</a></p>
+					<p><a href={'/#chat'}>chat</a></p>
+					<p><a href={'/#human-rights'}>human rights</a></p>
+					<p><a href={'/#invite-chris'}>invite Chris</a></p>
 				</div>
 				<div className={'uppercase text-lg space-y-5'}>
 					<p className={'font-semibold'}>Legal</p>
